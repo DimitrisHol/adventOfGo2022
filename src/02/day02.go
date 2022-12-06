132386e347938ad72979dbfaf351e3f790a9dabb
Dimitris Hol <dimitrishol@protonmail.com>
2022-12-06 19:05:52 +0200

    Day02 - Fast boi

--- src/02/day02.go
+++ src/02/day02.go
@@ -13,13 +13,13 @@ func parseFile(filename string) []string {
 	return strings.Split(stringData, "\r\n")
 }
 
-func part1Fast(data []string) int {
+func part1(data []string) int {
 
 	var totalScore int = 0
 	scoringPairs := map[string]int{
 		"A X": 4, "A Y": 8, "A Z": 3,
-		"B X": 1, "B Y": 5, "C Z": 6,
-		"C X": 7, "C Y": 2, "B Z": 9}
+		"B X": 1, "B Y": 5, "B Z": 9,
+		"C X": 7, "C Y": 2, "C Z": 6}
 
 	for _, pair := range data {
 		totalScore += scoringPairs[pair]
@@ -29,101 +29,20 @@ func part1Fast(data []string) int {
 
 func part2(data []string) int {
 
-	var totalScorePart int = 0
+	var totalScore int = 0
+	scoringPairs := map[string]int{
+		"A X": 3, "A Y": 4, "A Z": 8,
+		"B X": 1, "B Y": 5, "B Z": 9,
+		"C X": 2, "C Y": 6, "C Z": 7}
 
 	for _, pair := range data {
-		var localScore int = 0
-
-		picks := strings.Split(pair, " ")
-
-		opponentPick := picks[0]
-		strategy := picks[1]
-
-		// Step 1 : Follow the strategy :
-		if strategy == "X" { // LOSS :
-
-			if opponentPick == "A" {
-				strategy = "Z"
-			} else if opponentPick == "B" {
-				strategy = "X"
-			} else {
-				strategy = "Y"
-			}
-
-		} else if strategy == "Y" {
-
-			if opponentPick == "A" {
-				strategy = "X"
-			} else if opponentPick == "B" {
-				strategy = "Y"
-			} else {
-				strategy = "Z"
-			}
-
-		} else if strategy == "Z" {
-
-			if opponentPick == "A" {
-				strategy = "Y"
-			} else if opponentPick == "B" {
-				strategy = "Z"
-			} else {
-				strategy = "X"
-			}
-
-		}
-
-		// Step 2 : Bonus points
-		pickPoints := calculatePickPoints(strategy)
-		localScore += pickPoints
-
-		// Step 2 : Part 1 Win condition Resolution
-		var winCondition bool = strategy == "X" && opponentPick == "C" ||
-			strategy == "Y" && opponentPick == "A" ||
-			strategy == "Z" && opponentPick == "B"
-
-		var drawCondition bool = strategy == "X" && opponentPick == "A" ||
-			strategy == "Y" && opponentPick == "B" ||
-			strategy == "Z" && opponentPick == "C"
-
-		var lossCondition bool = strategy == "X" && opponentPick == "B" ||
-			strategy == "Y" && opponentPick == "C" ||
-			strategy == "Z" && opponentPick == "A"
-
-		// Step 3 : Award points :
-		if winCondition {
-			localScore += 6
-		} else if drawCondition {
-			localScore += 3
-		} else if lossCondition {
-		} else {
-			panic("Something went wrong")
-		}
-
-		totalScorePart += localScore
-
+		totalScore += scoringPairs[pair]
 	}
-
-	return totalScorePart
-
-}
-
-func calculatePickPoints(pick string) int {
-
-	if pick == "X" {
-		return 1
-	} else if pick == "Y" {
-		return 2
-	} else if pick == "Z" {
-		return 3
-	} else {
-		panic("Things went wrong")
-	}
+	return totalScore
 }
 
 func main() {
-
 	data := parseFile("../../input/day02.txt")
-	fmt.Printf("Part 1 Score : %v\n", part1Fast(data))
+	fmt.Printf("Part 1 Score : %v\n", part1(data))
 	fmt.Printf("Part 2 Score : %v\n", part2(data))
-
 }
