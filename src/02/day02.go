ca01331dff74301e4010eb70c573e5394588a2bc
Dimitris Hol <dimitrishol@protonmail.com>
2022-12-06 19:09:30 +0200

    Day02 - One pass

--- src/02/day02.go
+++ src/02/day02.go
@@ -13,36 +13,26 @@ func parseFile(filename string) []string {
 	return strings.Split(stringData, "\r\n")
 }
 
-func part1(data []string) int {
+func main() {
+	data := parseFile("../../input/day02.txt")
 
-	var totalScore int = 0
-	scoringPairs := map[string]int{
+	var part1, part2 int = 0, 0
+
+	part1Scores := map[string]int{
 		"A X": 4, "A Y": 8, "A Z": 3,
 		"B X": 1, "B Y": 5, "B Z": 9,
 		"C X": 7, "C Y": 2, "C Z": 6}
 
-	for _, pair := range data {
-		totalScore += scoringPairs[pair]
-	}
-	return totalScore
-}
-
-func part2(data []string) int {
-
-	var totalScore int = 0
-	scoringPairs := map[string]int{
+	part2Scores := map[string]int{
 		"A X": 3, "A Y": 4, "A Z": 8,
 		"B X": 1, "B Y": 5, "B Z": 9,
 		"C X": 2, "C Y": 6, "C Z": 7}
 
 	for _, pair := range data {
-		totalScore += scoringPairs[pair]
+		part1 += part1Scores[pair]
+		part2 += part2Scores[pair]
 	}
-	return totalScore
-}
 
-func main() {
-	data := parseFile("../../input/day02.txt")
-	fmt.Printf("Part 1 Score : %v\n", part1(data))
-	fmt.Printf("Part 2 Score : %v\n", part2(data))
+	fmt.Printf("Part 1 Score : %v\n", part1)
+	fmt.Printf("Part 2 Score : %v\n", part2)
 }
